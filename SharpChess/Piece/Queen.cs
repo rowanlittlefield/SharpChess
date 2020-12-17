7f52449183aff01148e1b5c13b80da94dde37441
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-12-16 19:53:08 -0500

    Remove obsolete parameters from GetMoveOptions method for remaining Piece subclasses

--- SharpChess/Piece/Queen.cs
+++ SharpChess/Piece/Queen.cs
@@ -13,7 +13,7 @@ namespace SharpChess
             return "Q";
         }
 
-        public override HashSet<(int, int)> GetMoveOptions(Board board, (int, int) coordinates)
+        public override HashSet<(int, int)> GetMoveOptions(Board board)
         {
             return new HashSet<(int, int)> { };
         }
--- SharpChess/Piece/Rook.cs
+++ SharpChess/Piece/Rook.cs
@@ -13,7 +13,7 @@ namespace SharpChess
             return "r";
         }
 
-        public override HashSet<(int, int)> GetMoveOptions(Board board, (int, int) coordinates)
+        public override HashSet<(int, int)> GetMoveOptions(Board board)
         {
             return new HashSet<(int, int)> { };
         }
