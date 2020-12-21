58a262aee38fed274680d813694a5ab3fd3536aa
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-12-21 15:51:51 -0500

    Implement GetMoveOptions methods on sliding pieces using SlidingPathFinder static class

--- SharpChess/Piece/Queen.cs
+++ SharpChess/Piece/Queen.cs
@@ -26,43 +26,7 @@ namespace SharpChess
 
         public override HashSet<(int, int)> GetMoveOptions(Board board)
         {
-            var moveOptions = new HashSet<(int, int)> { };
-            foreach (var moveDiff in MOVE_DIFFS)
-            {
-                var movesInDirection = _getMovesInDirection(board, moveDiff);
-                foreach (var position in movesInDirection)
-                {
-                    moveOptions.Add(position);
-                }
-            }
-
-            return moveOptions;
-        }
-
-        private HashSet<(int, int)> _getMovesInDirection(Board board, (int, int) moveDiff)
-        {
-            var moveOptions = new HashSet<(int, int)> { };
-            var position = Coordinates;
-            var (rowDiff, colDiff) = moveDiff;
-
-            var isPathClear = true;
-            while (isPathClear)
-            {
-                position = (position.Item1 + rowDiff, position.Item2 + colDiff);
-                var invalidMove = !board.IsOnBoard(position)
-                    || board.GetPiece(position).Color == Color;
-                if (invalidMove)
-                {
-                    isPathClear = false;
-                }
-                else
-                {
-                    moveOptions.Add(position);
-                    isPathClear = board.GetPiece(position).IsNullPiece();
-                }
-            }
-
-            return moveOptions;
+            return SlidingPathFinder.GetMoveOptions(board, this, MOVE_DIFFS);
         }
     }
 }
