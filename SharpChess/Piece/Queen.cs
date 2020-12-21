2badcd96342c9004976569f0018c472c67246c00
Rowan Littlefield <rowanlittlefield@ymail.com>
2020-12-21 11:43:40 -0500

    Implement rough draft of Queen movement

--- SharpChess/Piece/Queen.cs
+++ SharpChess/Piece/Queen.cs
@@ -4,6 +4,17 @@ namespace SharpChess
 {
     public class Queen : Piece
     {
+        private static readonly (int, int)[] MOVE_DIFFS = {
+            (1, 0),
+            (1, -1),
+            (0, -1),
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        };
+
         public Queen(PieceColor color, (int, int) coordinates) : base(color, coordinates)
         {
         }
@@ -15,7 +26,43 @@ namespace SharpChess
 
         public override HashSet<(int, int)> GetMoveOptions(Board board)
         {
-            return new HashSet<(int, int)> { };
+            var moveOptions = new HashSet<(int, int)> { };
+            foreach (var moveDiff in MOVE_DIFFS)
+            {
+                var movesInDirection = _getMovesInDirection(board, moveDiff);
+                foreach (var position in movesInDirection)
+                {
+                    moveOptions.Add(position);
+                }
+            }
+
+            return moveOptions;
+        }
+
+        private HashSet<(int, int)> _getMovesInDirection(Board board, (int, int) moveDiff)
+        {
+            var moveOptions = new HashSet<(int, int)> { };
+            var position = Coordinates;
+            var (rowDiff, colDiff) = moveDiff;
+
+            var isPathClear = true;
+            while (isPathClear)
+            {
+                position = (position.Item1 + rowDiff, position.Item2 + colDiff);
+                var invalidMove = !board.IsOnBoard(position)
+                    || board.GetPiece(position).Color == Color;
+                if (invalidMove)
+                {
+                    isPathClear = false;
+                }
+                else
+                {
+                    moveOptions.Add(position);
+                    isPathClear = board.GetPiece(position).IsNullPiece();
+                }
+            }
+
+            return moveOptions;
         }
     }
 }
